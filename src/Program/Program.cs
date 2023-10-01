997107b2b7de9064f6ff6a60d59451dccb15eee4
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-10-01 18:54:27 -0300

    implementacion del VP

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RoleplayGame;
+using System.Collections.Generic;
 
 namespace Program
 {
@@ -7,51 +8,33 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            SpellsBook book = new SpellsBook();
-            book.AddSpell(new SpellOne());
-            book.AddSpell(new SpellOne());
+            Hero hero1 = new Hero("Hero1", 0);
+            Hero hero2 = new Hero("Hero2", 0);
 
-            Wizard emi = new Wizard("Emily");
-            emi.AddItem(book);
-            Wizard maru = new Wizard("Mary");
-            maru.AddItem(book);
+            // Crear instancias de enemigos
+            Enemy enemy1 = new Enemy("Enemy1", 10);
+            Enemy enemy2 = new Enemy("Enemy2", 15);
 
-            Dwarf mateito = new Dwarf("Mathew");
-            Dwarf franco = new Dwarf("Frank");
-            
-            Console.WriteLine($"Mary attacks Mathew with ⚔️  {maru.AttackValue}");// maru ataca a mateo
-            mateito.ReceiveAttack(maru.AttackValue);
-            Console.WriteLine($"Mathew has ❤️  {mateito.Health}"); //el valor de salud de mateo
-            
-            Console.WriteLine($"Emily attacks Frank with ⚔️  {emi.AttackValue}");//emi ataca a franco
-            franco.ReceiveAttack(emi.AttackValue);
-            Console.WriteLine($"Frank has ❤️  {franco.Health}"); //el valor de salud de franco
-            
-            Console.WriteLine($"Frank attacks Mary with ⚔️  {franco.AttackValue}");//franco ataca a maru
-            maru.ReceiveAttack(franco.AttackValue);
-            Console.WriteLine($"Mary has ❤️  {maru.Health}"); //valor de salud de maru
-            
-            Console.WriteLine($"Mathew attacks Emily with ⚔️  {mateito.AttackValue}"); //mateito ataca a emi
-            emi.ReceiveAttack(mateito.AttackValue);
-            Console.WriteLine($"Emiliy has ❤️  {emi.Health}"); //valor de salud de emi
-            
-            mateito.Cure();
+             // Crear listas de héroes y enemigos
+            List<ICharacter> heroes = new List<ICharacter> { hero1, hero2 };
+            List<ICharacter> enemies = new List<ICharacter> { enemy1, enemy2 };
 
-            Console.WriteLine($"Someone cured Mathew. Mathew now has ❤️  {mateito.Health}");
+            // Crear una instancia de Encounter pasando las listas de héroes y enemigos
+            Encounter encounter = new Encounter(heroes, enemies);
 
-            Enemy lotso = new Enemy("Lotso", 100);
-            Enemy sauron = new Enemy("Sauron", 120);
-            Enemy ursula = new Enemy("Ursula", 180);
-            Enemy yzma = new Enemy("Yzma", 200);
+            // Llamar a DoEncounter() para ejecutar el encuentro
+            encounter.DoEncounter();
 
-            Console.WriteLine($"Lotso's VP is {lotso.VP}"); // Imprimo el VP de Lotso
-            Console.WriteLine($"Sauron's VP is {sauron.VP}"); // Imprimo el VP de Sauron
-            Console.WriteLine($"Ursula's VP is {ursula.VP}"); // Imprimo el VP de Ursula
-            Console.WriteLine($"Yzma's VP is {yzma.VP}"); // Imprimo el VP de Yzma
-
-            // agregar vp heros
+            // Imprimir los valores de VP después del encuentro
+            foreach (var hero in heroes)
+            {
+                Console.WriteLine($"{hero.Name}'s VP is {hero.VP}");
+            }
+            foreach (var enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.Name}'s VP is {enemy.VP}");
+            }
 
         }
     }
 }
-
