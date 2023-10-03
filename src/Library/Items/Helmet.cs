3414995195ae6c1bb7f24fcba183993ea4074f29
francofil <francofil03@gmail.com>
2023-10-03 20:56:24 -0300

    helmet

--- src/Library/Items/Helmet.cs
+++ src/Library/Items/Helmet.cs
@@ -1,33 +1,35 @@
-public class Helmet : IDefenseItem
-{
-    private int defenseValue;
-
-    public int DefenseValue
+namespace RoleplayGame{
+    public class Helmet : IDefenseItem
     {
-        get { return defenseValue; }
-        set
+        private int defenseValue;
+
+        public int DefenseValue
         {
-            // Validar que el valor de defensa no sea negativo
-            defenseValue = value < 0 ? 0 : value;
+            get { return defenseValue; }
+            set
+            {
+                // Validar que el valor de defensa no sea negativo
+                defenseValue = value < 0 ? 0 : value;
+            }
         }
-    }
 
-    public Helmet(int initialDefenseValue)
-    {
-        // Asignar el valor de defensa inicial, asegurándote de que no sea negativo
-        DefenseValue = initialDefenseValue;
-    }
-
-    public void ReduceDefense(int amount)
-    {
-        // Implementa la reducción de defensa aquí, si es necesario.
-        // Puedes elegir no hacer nada si el casco no debe reducir su defensa.
-        // Por ejemplo, podrías tener una lógica que reduzca la defensa del casco
-        // en función de la cantidad recibida como parámetro.
-        DefenseValue -= amount;
-        if (DefenseValue < 0)
+        public Helmet(int initialDefenseValue)
         {
-            DefenseValue = 0;
+            // Asignar el valor de defensa inicial, asegurándote de que no sea negativo
+            DefenseValue = initialDefenseValue;
+        }
+
+        public void ReduceDefense(int amount)
+        {
+            // Implementa la reducción de defensa aquí, si es necesario.
+            // Puedes elegir no hacer nada si el casco no debe reducir su defensa.
+            // Por ejemplo, podrías tener una lógica que reduzca la defensa del casco
+            // en función de la cantidad recibida como parámetro.
+            DefenseValue -= amount;
+            if (DefenseValue < 0)
+            {
+                DefenseValue = 0;
+            }
         }
     }
 }
